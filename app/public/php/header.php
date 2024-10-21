4337a7dab0bc9069077a4d3df5d1381eb9971ff1
rodove-tv <enzo.martinez.travail@gmail.com>
2024-10-21 15:27:10 +0200

    arrange the menu and the header

--- app/public/php/header.php
+++ app/public/php/header.php
@@ -1,4 +1,6 @@
-
-    <header>
-        <h1>Welcome to My CV</h1>
-    </header>
+<link rel="stylesheet" type="text/css" href="../../public/css/heder.css" />
+<header>
+    <img src="../../public/resource/logo.png" alt="">
+    <h1>Welcome to My CV</h1>
+    <?php include 'menu.php'; ?>
+</header>
